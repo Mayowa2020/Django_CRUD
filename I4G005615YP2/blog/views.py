f2a5e399c6081f34c404e925a70f6d93f2176a7c
Mayowa2020 <73591664+Mayowa2020@users.noreply.github.com>
2022-06-28 00:30:52 +0100

    Added the views

--- I4G005615YP2/blog/views.py
+++ I4G005615YP2/blog/views.py
@@ -1,3 +1,25 @@
+from dataclasses import fields
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from I4G005615YP2.blog.models import Post
 
 # Create your views here.
+
+class PostListView(generic.ListView):
+    model = Post
+
+class PostCreateView(generic.CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDetailView(generic.DetailView):
+    model = Post
+
+class PostUpdateView(generic.UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+    class PostDeleteView(generic.UpdateView):    
+
